a7e20939d94f87198d1a9dcc0b37d01fc452a29c
lujianxin <email@lujianxin.cn>
2019-01-23 14:56:31 +0800

    修改开机自启脚本

--- docs/api.sh
+++ docs/api.sh
@@ -2,6 +2,6 @@
 #chkconfig: 2345 80 90
 #description: auto_run
 # 配置webhook服务开机自启
-source /root/venvs/webhook/bin/activate
-cd /root/apps/robot/
+source /root/venvs/api/bin/activate
+cd /root/apps/api/
 gunicorn -c config.py app:app
