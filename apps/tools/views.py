1f592c7ef0e5a94fd3d86251d6d265ac19098f32
lujianxin <jianxin.lu@woqutech.com>
2019-07-08 15:22:41 +0800

    增加日志

--- apps/tools/views.py
+++ apps/tools/views.py
@@ -3,13 +3,17 @@ __descriptions__ = """
     一些工具接口
 """
 
+import logging
 import json
 
 from flask import Response, request, render_template
 from flask.views import MethodView
 
+logger = logging.getLogger(__name__)
+
 
 def http_echo_view():
-    json_data = request.get_json()
-    response = Response(json.dumps(json_data), mimetype='application/json')
+    json_data = json.dumps(request.get_json())
+    logger.info(json_data)
+    response = Response(json_data, mimetype='application/json')
     return response
